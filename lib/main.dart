271fccb273f2e52db87f37b8a2ad80ab4e27fb57
Jesus Rojas <jesusmrojas5991@gmail.com>
2023-04-30 01:21:43 -0700

    implemented table_calendar api. Month view is active but other views are not active yet, still unfinished. Current day is seen and select day is seen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 /*
 App Drawer Link: https://docs.flutter.dev/cookbook/design/drawer
@@ -20,6 +21,12 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: "Niche",
       debugShowCheckedModeBanner: false,
+      /*  Dark Mode Option Implementation: 
+      https://api.flutter.dev/flutter/material/MaterialApp/themeMode.html
+  
+      themeMode: ThemeMode.system,
+      theme: MyThemes.lightTheme,
+      darkTheme: MyThemes.darkTheme, */
       home: HomePage(title: 'HomePage'),
     );
   }
@@ -38,14 +45,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CalendarFormat format = CalendarFormat.month; //Month State
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          title: const Text("Niche"),
+          centerTitle: true,
           backgroundColor: Colors.black,
         ),
-        body: const Center(
+        body: TableCalendar(
+          firstDay: DateTime.utc(2015),
+          lastDay: DateTime.utc(2035),
+          focusedDay: DateTime.now(),
+          calendarFormat: format,
+          /*Create the option to change format
+            onFormatChanged: (CalendarFormat format) {
+              setState(() {
+                format: format;
+              });
+            },*/
+          //Option to select day with taps and color customization
+          selectedDayPredicate: (day) {
+            return isSameDay(selectedDay, day);
+          },
+          onDaySelected: (DateTime selectDay, DateTime focusDay) {
+            setState(() {
+              selectedDay = selectDay;
+              focusedDay = focusDay;
+            });
+          },
+          //Calendar Style:
+          calendarStyle: const CalendarStyle(
+            isTodayHighlighted: true,
+              selectedDecoration: BoxDecoration(
+                color: Colors.purple,
+                shape: BoxShape.rectangle,
+              ),
+              selectedTextStyle: TextStyle(color: Colors.white),
+            todayDecoration: BoxDecoration(
+              color: Colors.red,
+              shape: BoxShape.rectangle,
+            ),
+            todayTextStyle: TextStyle(color: Colors.white),
+          ),
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            formatButtonShowsNext: false,
+          ),
+          /*
+          const Center(
           child: Text('Calendar Coming Soon...'),
+          */
         ),
         drawer: Drawer(
           child: ListView(padding: EdgeInsets.zero, children: [
@@ -65,6 +120,7 @@ class _HomePageState extends State<HomePage> {
               title: const Text('Week'),
               onTap: () {
                 Navigator.pop(context);
+                //CalendarFormat: format;
               },
             ),
             ListTile(
